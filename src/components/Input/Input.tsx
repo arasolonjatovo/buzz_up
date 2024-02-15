0b91f4641c5965187f3b5c9c94d681ef5de24293
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2024-02-15 22:03:16 +0100

    CREATE new component : Input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,46 @@
+import React, { ChangeEvent, RefObject } from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import './Input.scss'
+
+type Props = {
+  label?: string
+  placeholder?: string
+  name?: string
+  id?: string
+  value?: string | number
+  type?: string
+  inputRef?: RefObject<HTMLInputElement>
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+  required?: boolean
+  icon?: any
+  onClickIcon?: () => void
+}
+
+export default function Input({ label, placeholder, name, id, value, type, inputRef, onChange, required, icon, onClickIcon }: Props) {
+  return (
+    <div className="input__container">
+      <label htmlFor={label}>{label}</label>
+      <div className="input">
+        <input
+          placeholder={placeholder}
+          name={name}
+          id={id}
+          value={value}
+          type={type}
+          ref={inputRef}
+          onChange={onChange}
+          required={required}
+        />
+        {icon && (
+          <FontAwesomeIcon
+            className="input-icon"
+            onClick={onClickIcon}
+            icon={icon}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
