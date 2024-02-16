d69a1449f838e529ccc6f5fe3d01e87ee4b9da6f
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2024-02-16 22:47:57 +0100

    FIX bad spelling

---
+++ src/routes/Todos/Todos.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '<div className="" />
+<Todos></Todos>.scss'
+
+type Props = {}
+
+export default function Todos({}: Props) {
+  return (
+    <div>Todods</div>
+  )
+}
