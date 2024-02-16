5889a1ff54036bcce088432a17d4df7254b659a4
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2024-02-16 23:13:12 +0100

    CREATE privateRoute

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { UserContext } from '../context/userContext'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export default function PrivateRoute({ children }: Props) {
+  const { userID } = useContext(UserContext)
+  const navigation = useNavigate()
+
+  useEffect(() => {
+    if (userID !== 'null') {
+      navigation('/signIn')
+    }
+  }, [userID, navigation])
+
+  return <>{children}</>
+}
