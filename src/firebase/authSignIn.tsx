96e0e68e1d1bfc0d920ef8b71a4ccd0af2895eea
lsun <lsun.github@gmail.com>
2024-02-17 15:08:11 +0100

    add: init auth but not style yet

---
+++ src/firebase/authSignIn.tsx
@@ -0,0 +1,22 @@
+import { signInWithEmailAndPassword, UserCredential } from 'firebase/auth'
+import { auth } from './firebase'
+
+export function authSignIn(email: string, password: string): Promise<UserCredential['user']> {
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential: UserCredential) => {
+      const user = userCredential.user
+      console.log('User logged in:', user)
+      return user
+    })
+    .catch((error: { message: string; code: string }) => {
+      console.error(error.message, error.code)
+      if (
+        error.code === 'auth/user-not-found' ||
+        error.code === 'auth/wrong-password'
+      ) {
+        throw new Error('Invalid email or password. Please try again.')
+      } else {
+        throw error
+      }
+    })
+}
